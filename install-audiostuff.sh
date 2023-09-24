cc89219cd0f801fd9a31b61252c5bf0e85050b4f
Levente Botos <botos.levente2007@gmail.com>
2023-09-24 16:32:30 +0200

    improv

--- install-audiostuff.sh
+++ install-audiostuff.sh
@@ -1,16 +1,15 @@
-apt-get update
-apt-get upgrade
+mkdir audio stuff
+sudo apt-get update
+sudo apt-get upgrade
 
-apt install --no-install-recommends build-essential git autoconf automake libtool \
-    libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev \
-    libplist-dev libsodium-dev libavutil-dev libavcodec-dev libavformat-dev uuid-dev libgcrypt-dev xxd
+sudo apt install --no-install-recommends build-essential git autoconf automake libtool libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev libplist-dev libsodium-dev libavutil-dev libavcodec-dev libavformat-dev uuid-dev libgcrypt-dev xxd
 
 git clone https://github.com/mikebrady/nqptp.git
 cd nqptp
 autoreconf -fi
 ./configure --with-systemd-startup
-make 
-make install
+sudo make 
+sudo make install
 
 systemctl enable nqptp
 systemctl start nqptp
@@ -19,8 +18,7 @@ cd ../
 git clone https://github.com/mikebrady/shairport-sync.git
 cd shairport-sync
 autoreconf -fi
-./configure --sysconfdir=/etc --with-alsa \
-    --with-soxr --with-avahi --with-ssl=openssl --with-systemd --with-airplay-2
+./configure --sysconfdir=/etc --with-alsa --with-soxr --with-avahi --with-ssl=openssl --with-systemd --with-airplay-2
 make
 make install
 
